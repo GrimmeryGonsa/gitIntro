ae42f153ffb7bde241dda3b9479d7da7a3ea156f
GrimmeryGonsa <saumya2234@gmail.com>
2020-09-03 21:33:58 +0530

    sheldon greets added...

--- file02.py
+++ file02.py
@@ -1,11 +1,10 @@
 print("ey we are learnin git....")
 
-def greet(name):
-	print("HELLO"+name)
+def Sheldon(sr):
+	for i in range(3):
+		print("KNOCK KNOCK KNOCk "+ sr)
 
-
-greet(Saumya)
+Sheldon('Sam')
 
 #use git status to check status 
 #use git log to check log/history
-
